b543b7b0cbe8235eb8ecb32e8c6995a5a1d86bbc
lachlanleslie1995 <lachlan.leslie1995@gmail.com>
2019-05-07 23:49:42 +1000

    Trying to add firebase shit

--- app/src/main/java/com/example/studio1bgroup11/edu2/LoginMainActivity.java
+++ app/src/main/java/com/example/studio1bgroup11/edu2/LoginMainActivity.java
@@ -1,72 +1,181 @@
 package com.example.studio1bgroup11.edu2;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginMainActivity extends AppCompatActivity {
-    SQLiteDatabase db;
-    SQLiteOpenHelper openHelper;
-    Cursor cursor;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
+
+public class LoginMainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
     Button loginBtn, registerBtn;
-    EditText usernameEt, passwordEt;
+    EditText mEmailField, mPasswordField;
     TextView registerTv;
 
+    private static final String TAG = "EmailPassword";
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +         //at least 1 digit
+                    "(?=.*[a-z])" +         //at least 1 lowercase letter
+                    "(?=.*[A-Z])" +         //at least 1 uppercase letter
+                    //"(?=.*[a-zA-Z])" +    //any letter
+                    //"(?=.*[@#$%^&+=])" +  //at least 1 special character
+                    "(?=\\S+$)" +             //no white spaces
+                    ".{6,}" +               //at least 6 characters
+                    "$");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
-        usernameEt = (EditText) findViewById(R.id.usernameEditText);
-        passwordEt = (EditText) findViewById(R.id.passwordEditText);
-        loginBtn = (Button) findViewById(R.id.loginBtn);
-        openHelper = new DatabaseHelper(this);
-        db = openHelper.getReadableDatabase();
-        registerTv = (TextView) findViewById(R.id.registertextView);
-        registerBtn = (Button) findViewById(R.id.registerBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        /* Views */
+        mEmailField = findViewById(R.id.usernameEditText);
+        mPasswordField = findViewById(R.id.passwordEditText);
+        loginBtn = findViewById(R.id.loginBtn);
+        registerTv = findViewById(R.id.registertextView);
+
+        /* Buttons */
+        registerBtn = findViewById(R.id.registerBtn);
         loginBtn.setEnabled(false);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = usernameEt.getText().toString();
-                String password = passwordEt.getText().toString();
-                cursor =db.rawQuery("SELECT * FROM "+DatabaseHelper.TABLE_NAME +" WHERE "
-                        +DatabaseHelper.COL_2 +" =? AND " +DatabaseHelper.COL_3 +" =? ",new String[]{username, password});
-                if(cursor !=null)
-                {
-                    if (cursor.getCount() > 0) {
 
-                        cursor.moveToNext();
-                        Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginMainActivity.this, HomeActivity.class);
-                        startActivity(intent);
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Username and/or Password Incorrect.", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }});
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginMainActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-        usernameEt.addTextChangedListener(loginTextWatcher);
-        passwordEt.addTextChangedListener(loginTextWatcher);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            sendEmailVerification();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginMainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginMainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendEmailVerification() {
+        // Send verification email
+        final FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginMainActivity.this,
+                                        "Verification email sent to " + user.getEmail(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.e(TAG, "sendEmailVerification", task.getException());
+                                Toast.makeText(LoginMainActivity.this,
+                                        "Failed to send verification email.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailField.setError("Invalid Email.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password) && !PASSWORD_PATTERN.matcher(password).matches()) {
+            mPasswordField.setError("Invalid password.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == -1/*R.id.emailCreateAccountButton*/) {
+            createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.loginBtn) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        }
+    }
+
     private TextWatcher loginTextWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -75,8 +184,8 @@ public class LoginMainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            String usernameInput = usernameEt.getText().toString().trim();
-            String passwordInput = passwordEt.getText().toString().trim();
+            String usernameInput = mEmailField.getText().toString().trim();
+            String passwordInput = mPasswordField.getText().toString().trim();
             loginBtn.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty());
         }
 
